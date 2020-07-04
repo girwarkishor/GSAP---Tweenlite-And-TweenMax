7d508800d455a6cb7a8bbd6b3c00c79205ab5c59
girwarkishor <girwar.kishor@gmail.com>
2020-07-04 16:56:07 +0530

    from() and fromTo() properties

--- function.js
+++ function.js
@@ -2,4 +2,16 @@
 // gsap.to("img", { x: 400, duration: 3 });
 // gsap.to("img", { x: 400, y: 200, scale: 3, duration: 3 });
 // gsap.to("img", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 }); //For all img tag
-gsap.to(".fred", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 });
+// gsap.to(".fred", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 });
+// gsap.from(".fred", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 });
+// gsap.fromTo(".fred", { x: 700, y: 400 }, { x: 400, y: 200, duration: 3 });
+// gsap.fromTo(
+//   ".fred",
+//   { x: 700, y: 400, opacity: 0 },
+//   { x: 400, y: 200, scale: 2, duration: 3 }
+// ); // Show nothing as you add opacity: 0
+gsap.fromTo(
+  ".fred",
+  { x: 700, y: 400, opacity: 0 },
+  { x: 400, y: 200, scale: 2, opacity: 1, duration: 3 }
+);
