e48516df89666de544690205962565a8b4628563
girwarkishor <girwar.kishor@gmail.com>
2020-07-04 17:06:22 +0530

    Repeat and delay properties

--- function.js
+++ function.js
@@ -10,8 +10,20 @@
 //   { x: 700, y: 400, opacity: 0 },
 //   { x: 400, y: 200, scale: 2, duration: 3 }
 // ); // Show nothing as you add opacity: 0
-gsap.fromTo(
-  ".fred",
-  { x: 700, y: 400, opacity: 0 },
-  { x: 400, y: 200, scale: 2, opacity: 1, duration: 3 }
-);
+// gsap.fromTo(
+//   ".fred",
+//   { x: 700, y: 400, opacity: 0 },
+//   { x: 400, y: 200, scale: 2, opacity: 1, duration: 3 }
+// );
+
+// gsap.to(".green", { duration: 2, x: 600 });
+// gsap.to(".pink", { duration: 2, x: 600, delay: 2 }); // Delay
+
+// gsap.to(".green", { duration: 2, x: 600, repeat: 1 }); // Repeat
+// gsap.to(".pink", { duration: 2, x: 600 });
+
+// gsap.to(".green", { duration: 2, x: 600, repeat: 1, yoyo: true }); // yoyo
+// gsap.to(".pink", { duration: 2, x: 600 });
+
+gsap.to(".green", { duration: 2, x: 600, repeat: -1, yoyo: true }); // repeat: -1; repeat for ever
+gsap.to(".pink", { duration: 2, x: 600 });
