530b72d99beab4f7bfac71e4bb023e21a71da45f
girwarkishor <girwar.kishor@gmail.com>
2020-07-04 16:25:49 +0530

    Basic Tween

--- function.js
+++ function.js
@@ -1,1 +1,5 @@
-gsap.to("img", { x: 400 });
+// gsap.to("img", { x: 400 }); //Default during 0.5s
+// gsap.to("img", { x: 400, duration: 3 });
+// gsap.to("img", { x: 400, y: 200, scale: 3, duration: 3 });
+// gsap.to("img", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 }); //For all img tag
+gsap.to(".fred", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 });
